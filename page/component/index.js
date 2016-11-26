3eb934ac01da953a514631b58c55cf6ac8188c43
Melin <554203306@qq.com>
2016-11-26 12:22:12 +0800

    Music.

--- page/component/index.js
+++ page/component/index.js
@@ -1,15 +1,69 @@
-var pageData = {},
-    type = [
+
+Page({
+    widgetsToggle:function (e) {
+    var id = e.currentTarget.id, data = {};
+    var type = [
         'view', 'content', 'form', 'interact', 'nav', 'media', 'map', 'canvas'
     ];
-
-pageData.widgetsToggle = function (e) {
-    var id = e.currentTarget.id, data = {};
     for (var i = 0, len = type.length; i < len; ++i) {
         data[type[i] + 'Show'] = false;
     }
     data[id + 'Show'] = !this.data[id + 'Show'];
     this.setData(data);
-};
-
-Page(pageData);
+    },
+    data: {
+        imgUrls: [      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'],
+        indicatorDots: true,
+        vertical: false,
+        autoplay: false,
+        interval: 3000,
+        duration: 1000,
+        poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
+        name: '此时此刻',
+        author: '许巍',
+        src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46'
+    },
+    changeIndicatorDots: function (e) {
+        this.setData({
+            indicatorDots: !this.data.indicatorDots
+        })
+    },
+    changeVertical: function (e) {
+        this.setData({
+            vertical: !this.data.vertical
+        })
+    },
+    changeAutoplay: function (e) {
+        this.setData({
+            autoplay: !this.data.autoplay
+        })
+    },
+    intervalChange: function (e) {
+        this.setData({
+            interval: e.detail.value
+        })
+    },
+    durationChange: function (e) {
+        this.setData({
+            duration: e.detail.value
+        })
+    },
+    onReady: function (e) {
+    // 使用 wx.createAudioContext 获取 audio 上下文 context
+    this.audioCtx = wx.createAudioContext('myAudio')
+  },
+  audioPlay: function () {
+    this.audioCtx.play()
+  },
+  audioPause: function () {
+    this.audioCtx.pause()
+  },
+  audio14: function () {
+    this.audioCtx.seek(14)
+  },
+  audioStart: function () {
+    this.audioCtx.seek(0)
+  }
+})
